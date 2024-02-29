3f84920bc662679633fff2536da50dba128b5dda
Dong She <dshe922@gmail.com>
2024-02-29 12:08:23 +0100

    update

--- src/components/design/BubbleChartTimeline.js
+++ src/components/design/BubbleChartTimeline.js
@@ -6,7 +6,6 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Button from 'react-bootstrap/Button';
 import scriptTemplate from "./scriptTemplate";
-import { data } from "@tensorflow/tfjs";
 
 const dataGroupColors = [
     "blue",
